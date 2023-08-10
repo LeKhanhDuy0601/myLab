174dd6d22f3811b4cf0be9d9769ebe15d1880162
LeKhanhDuy0601 <duylkpc01980@fpt.edu.vn>
2023-08-10 15:35:18 +0700

    duy delete

--- lab5_J6_duylkpc01980/src/main/java/com/j6Lab5/dao/OrderDao.java
+++
@@ -1,9 +0,0 @@
-package com.j6Lab5.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.j6Lab5.entity.Order;
-
-public interface OrderDao  extends JpaRepository<Order, Long>{
-
-}
--- lab5_J6_duylkpc01980/src/main/java/com/j6Lab5/dao/OrderDetailDao.java
+++
@@ -1,9 +0,0 @@
-package com.j6Lab5.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.j6Lab5.entity.OrderDetail;
-
-public interface OrderDetailDao extends JpaRepository<OrderDetail, Long> {
-
-}
